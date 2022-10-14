0056fc694c01cd007787293f3f08b94e23b575d8
t-kimura1988 <goen.business.managament@gmail.com>
2022-10-14 15:46:43 +0900

    Tracking description add

--- daiku-app-ios/daiku_app_iosApp.swift
+++ daiku-app-ios/daiku_app_iosApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import Firebase
 import GoogleSignIn
+import AppTrackingTransparency
 
 @main
 struct daiku_app_iosApp: App {
@@ -21,13 +22,34 @@ struct daiku_app_iosApp: App {
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        requestIDFA()
         
         FirebaseApp.configure()
-        
         return true
     }
     
     func applicationWillTerminate(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
         return GIDSignIn.sharedInstance.handle(url)
+    }
+    
+    func requestIDFA() {
+        if #available(iOS 15, *) {
+            print("ios15....")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                
+              ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                  print(status.rawValue)
+              })
+            }
+            
+        } else if #available(iOS 14, *) {
+            print("ios14....")
+            
+            ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+              // Tracking authorization completed. Start loading ads here.
+              // loadAd()
+            })
+            
+        }
     }
 }
